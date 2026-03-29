ed414c1b9bd8bab5761e078df623b6c19bb7417f
Ameb8 <alexmeb81@outlook.com>
2026-03-29 14:16:51 -0700

    chore(frontend): Updated Vite config to server onig.wasm static files via vite-plugin-static-copy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/vscode-oniguruma/release/onig.wasm',
+          dest: 'assets',
+        },
+        {
+          src: 'src/monaco/grammars/latex.tmLanguage.json',
+          dest: 'assets/grammars',
+        },
+      ],
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
