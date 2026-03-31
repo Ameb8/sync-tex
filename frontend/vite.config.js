6bb65c79094585f2e19b3f76e9474f2c769ddcee
Ameb8 <alexmeb81@outlook.com>
2026-03-30 22:26:37 -0700

    chore(frontend): Removed vite static copy logic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,18 +5,6 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/vscode-oniguruma/release/onig.wasm',
-          dest: 'assets',
-        },
-        {
-          src: 'src/monaco/grammars/latex.tmLanguage.json',
-          dest: 'assets/grammars',
-        },
-      ],
-    }),
   ],
   server: {
     host: '0.0.0.0',
